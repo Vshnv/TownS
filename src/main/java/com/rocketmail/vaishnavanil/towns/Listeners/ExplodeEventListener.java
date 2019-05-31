b1bc4890b5181dbecd7f80becdffd698fd2e176c
Rishabh Raghunath <rishabh.r@zohocorp.com>
2019-05-31 23:06:41 +0530

    removed unused imports

--- src/main/java/com/rocketmail/vaishnavanil/towns/Listeners/ExplodeEventListener.java
+++ src/main/java/com/rocketmail/vaishnavanil/towns/Listeners/ExplodeEventListener.java
@@ -3,27 +3,18 @@ package com.rocketmail.vaishnavanil.towns.Listeners;
 import com.rocketmail.vaishnavanil.towns.TownS;
 import com.rocketmail.vaishnavanil.towns.Towns.Claim;
 import com.rocketmail.vaishnavanil.towns.Towns.Flag;
-import com.rocketmail.vaishnavanil.towns.Towns.Town;
-import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockExplodeEvent;
-import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ExplodeEventListener implements Listener {
 
     @EventHandler
-    public void onExplosionByEntity(EntityExplodeEvent event){
-        if(TownS.g().isClaimed(event.getEntity().getChunk())){
+    public void onExplosionByEntity(EntityExplodeEvent event) {
+        if (TownS.g().isClaimed(event.getEntity().getChunk())) {
             Claim claim = TownS.g().getClaim(event.getEntity().getChunk());
-            if(!claim.hasFlag(Flag.EXPLOSION)){
+            if (!claim.hasFlag(Flag.EXPLOSION)) {
                 event.setYield(0);
                 event.setCancelled(true);
             }
@@ -31,10 +22,10 @@ public class ExplodeEventListener implements Listener {
     }
 
     @EventHandler
-    public void onBlockExplode(EntityExplodeEvent event){
+    public void onBlockExplode(EntityExplodeEvent event) {
         //List<BlockState> blockStateList = new ArrayList<>();
-        for(Block block: event.blockList()){
-            if(TownS.g().isClaimed(block.getChunk())){
+        for (Block block : event.blockList()) {
+            if (TownS.g().isClaimed(block.getChunk())) {
                 event.setCancelled(true);
                 return;
             }
--- src/main/java/com/rocketmail/vaishnavanil/towns/Listeners/MoveEventListener.java
+++ src/main/java/com/rocketmail/vaishnavanil/towns/Listeners/MoveEventListener.java
@@ -2,20 +2,17 @@ package com.rocketmail.vaishnavanil.towns.Listeners;
 
 import com.rocketmail.vaishnavanil.towns.TownS;
 import com.rocketmail.vaishnavanil.towns.Towns.Claim;
-import com.rocketmail.vaishnavanil.towns.Towns.Town;
 import com.rocketmail.vaishnavanil.towns.Utilities.TitleSender;
-import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
 
 public class MoveEventListener implements Listener {
 
     @EventHandler
-    public void onPlayerMove(org.bukkit.event.player.PlayerMoveEvent event){
-        if( event.getFrom().getChunk() != event.getTo().getChunk() ){
+    public void onPlayerMove(org.bukkit.event.player.PlayerMoveEvent event) {
+        if (event.getFrom().getChunk() != event.getTo().getChunk()) {
 
             Player player = event.getPlayer();
             Chunk fromChunk = event.getFrom().getChunk();
@@ -27,25 +24,25 @@ public class MoveEventListener implements Listener {
             Boolean fromChunkClaimed = TownS.g().isClaimed(fromChunk);
             Boolean toChunkClaimed = TownS.g().isClaimed(toChunk);
 
-            if( !toChunkClaimed && fromChunkClaimed ){
+            if (!toChunkClaimed && fromChunkClaimed) {
                 //Player Entered Wilderness from Claimed Region
                 TitleSender.INSTANCE.sendTitle(player, "", "&2Entering Wilderness");
-            }else if( toChunkClaimed && !fromChunkClaimed ){
+            } else if (toChunkClaimed && !fromChunkClaimed) {
                 //Player Entered Claimed Area from Wilderness
                 toClaim.setName("l33t Plot Name");
                 // ^ Setting Name is just for testing.
                 String plot_owner = toClaim.getOwner().getName();
                 String plot_name = toClaim.getName();
                 String plot_town = toClaim.getTown().getName();
-                TitleSender.INSTANCE.sendTitle(player, "&a"+plot_name, "&cTown: &6"+ plot_town+" &8&l| &eOwner: &6"+plot_owner);
-            }else if( toChunkClaimed && fromChunkClaimed){
+                TitleSender.INSTANCE.sendTitle(player, "&a" + plot_name, "&cTown: &6" + plot_town + " &8&l| &eOwner: &6" + plot_owner);
+            } else if (toChunkClaimed && fromChunkClaimed) {
                 String plot_owner = toClaim.getOwner().getName();
                 String plot_name = toClaim.getName();
                 String plot_town = toClaim.getTown().getName();
-                if(plot_name.equals("")){
-                    TitleSender.INSTANCE.sendTitle(player, "", "&2Owner: &a"+plot_owner);
-                }else{
-                    TitleSender.INSTANCE.sendTitle(player, "", "&2Area: &a"+ plot_name+" &8&l| &eOwner: &6"+plot_owner);
+                if (plot_name.equals("")) {
+                    TitleSender.INSTANCE.sendTitle(player, "", "&2Owner: &a" + plot_owner);
+                } else {
+                    TitleSender.INSTANCE.sendTitle(player, "", "&2Area: &a" + plot_name + " &8&l| &eOwner: &6" + plot_owner);
                 }
             }
 
