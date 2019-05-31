d0559455e8132bbd76db3afe063577b66dc871ba
Vshnv <vaishnav.anil@rocketmail.com>
2019-05-31 16:44:53 +0530

    Added rank register

--- src/main/java/com/rocketmail/vaishnavanil/towns/TownS.java
+++ src/main/java/com/rocketmail/vaishnavanil/towns/TownS.java
@@ -2,8 +2,10 @@ package com.rocketmail.vaishnavanil.towns;
 
 import com.rocketmail.vaishnavanil.towns.Commands.PlotCmd;
 import com.rocketmail.vaishnavanil.towns.Commands.TownCmd;
+import com.rocketmail.vaishnavanil.towns.Configurations.ConfigManager;
 import com.rocketmail.vaishnavanil.towns.MapGUI.InvClickListen;
 import com.rocketmail.vaishnavanil.towns.Towns.Claim;
+import com.rocketmail.vaishnavanil.towns.Towns.Rank;
 import com.rocketmail.vaishnavanil.towns.Towns.Town;
 import org.bukkit.Chunk;
 import org.bukkit.command.CommandExecutor;
@@ -11,119 +13,148 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import javax.xml.bind.Marshaller;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public final class TownS extends JavaPlugin {
     //SINGLETON
     private static TownS instance;
-    public static TownS g(){
+
+    public static TownS g() {
         return instance;
     }
+
     //SINGLETON
     public static String PREFIX = "[TownS]";
-//MAPPING
+    //MAPPING
+    //Private/*CLAIM MAP*/ HashMap<Claim,Town> CM = new HashMap<>();
 
+    private/*TOWN MAP*/ HashMap<String, Town> TM = new HashMap<>();
 
-  //  private/*CLAIM MAP*/ HashMap<Claim,Town> CM = new HashMap<>();
-
-
-    private/*TOWN MAP*/ HashMap<String,Town> TM = new HashMap<>();
-
-    private/*CLAIM MAP*/ HashMap<String,Claim> Map = new HashMap<>();//FORMAT :: KEY ->  ChunkX::ChunkZ::WORLD
+    private/*CLAIM MAP*/ HashMap<String, Claim> Map = new HashMap<>();//FORMAT :: KEY ->  ChunkX::ChunkZ::WORLD
 
     private/*P-T Map*/ HashMap<UUID, Town> quickPlayer = new HashMap<>();
 
-    //
-    public void/*ADD CLAIM TO CLAIM MAP*/ aCtT(Claim claim){
-       // CM.put(claim,town);
-        Map.put(claim.x()+"::"+claim.z()+""+claim.getWorldName(),claim);
+    private HashMap<String, Rank> RankList = new HashMap<>();
+
+
+    public void/*ADD CLAIM TO CLAIM MAP*/ aCtT(Claim claim) {
+        // CM.put(claim,town);
+        Map.put(claim.x() + "::" + claim.z() + "" + claim.getWorldName(), claim);
     }
 
-    public void/*REMOVE CLAIM FROM CLAIM MAP*/rCfT(Claim claim){
-        Map.remove(claim.x()+"::"+claim.z()+"::"+claim.getWorldName());
+    public void/*REMOVE CLAIM FROM CLAIM MAP*/rCfT(Claim claim) {
+        Map.remove(claim.x() + "::" + claim.z() + "::" + claim.getWorldName());
         //CM.remove(claim);
     }
-    public Town getTown(Player p){
-        if(quickPlayer.containsKey(p.getUniqueId()))return quickPlayer.get(p.getUniqueId());
+
+    public Town getTown(Player p) {
+        if (quickPlayer.containsKey(p.getUniqueId())) return quickPlayer.get(p.getUniqueId());
         //ADD TO QUICK LOOKUP
-        for(Town m:TM.values()){
-            if(m.belongs(p)){quickPlayer.put(p.getUniqueId(),m);return m;}
+        for (Town m : TM.values()) {
+            if (m.belongs(p)) {
+                quickPlayer.put(p.getUniqueId(), m);
+                return m;
+            }
         }
         return null;
     }
-    public boolean hasTown(Player player){
+
+    public boolean hasTown(Player player) {
         try {
             return getTown(player) != null;
-        }catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
-    public void rPfT(Player player){
+
+    public void rPfT(Player player) {
         quickPlayer.remove(player.getUniqueId());
     }
-    public void rPfT(UUID id){
+
+    public void rPfT(UUID id) {
         quickPlayer.remove(id);
     }
-    public boolean isClaimed(Chunk chunk){
-        return isClaimed(chunk.getX(),chunk.getZ(),chunk.getWorld().getName());
+
+    public boolean isClaimed(Chunk chunk) {
+        return isClaimed(chunk.getX(), chunk.getZ(), chunk.getWorld().getName());
     }
-    public boolean isClaimed(int CX,int CZ,String wName){
-        if( Map.containsKey(CX+"::"+CZ+"::"+wName)){
-            Claim claim = Map.get(CX+"::"+CZ+"::"+wName);
-            try{
-            if(claim.getTown() == null){
-                Map.remove(CX+"::"+CZ+"::"+wName);
-                return false;
-            }}catch(Exception e){
-                Map.remove(CX+"::"+CZ+"::"+wName);
+
+    public boolean isClaimed(int CX, int CZ, String wName) {
+        if (Map.containsKey(CX + "::" + CZ + "::" + wName)) {
+            Claim claim = Map.get(CX + "::" + CZ + "::" + wName);
+            try {
+                if (claim.getTown() == null) {
+                    Map.remove(CX + "::" + CZ + "::" + wName);
+                    return false;
+                }
+            } catch (Exception e) {
+                Map.remove(CX + "::" + CZ + "::" + wName);
                 return false;
             }
         }
-        return Map.containsKey(CX+"::"+CZ+"::"+wName);
+        return Map.containsKey(CX + "::" + CZ + "::" + wName);
     }
-    public Claim getClaim(Chunk chunk){
-        return Map.get(chunk.getX()+"::"+chunk.getZ()+"::"+chunk.getWorld().getName());
+
+    public Claim getClaim(Chunk chunk) {
+        return Map.get(chunk.getX() + "::" + chunk.getZ() + "::" + chunk.getWorld().getName());
     }
-    public boolean townExists(String name){
+
+    public boolean townExists(String name) {
         return TM.keySet().contains(name);
     }
-    public Town getTown(String name){
-        if(TM.containsKey(name))return TM.get(name);
+
+    public Town getTown(String name) {
+        if (TM.containsKey(name)) return TM.get(name);
         return null;
     }
-    public Town getTown(Chunk chunk){
+
+    public Town getTown(Chunk chunk) {
         return getClaim(chunk).getTown();
     }
+
     //
-public void registerCMD(String cmd, CommandExecutor e){
+    public void registerCMD(String cmd, CommandExecutor e) {
         this.getServer().getPluginCommand(cmd).setExecutor(e);
-}
-public void regListen(Listener lis){
-        getServer().getPluginManager().registerEvents(lis,this);
-}
-    //
-    public void/*ADD TOWN TO MAP*/ aTtM(Town town){
-        TM.put(town.getName(),town);
     }
-    public void/*ADD TOWN FROM MAP*/ rTfM(Town town){
+
+    public void regListen(Listener lis) {
+        getServer().getPluginManager().registerEvents(lis, this);
+    }
+
+    //
+    public void/*ADD TOWN TO MAP*/ aTtM(Town town) {
+        TM.put(town.getName(), town);
+    }
+
+    public void/*ADD TOWN FROM MAP*/ rTfM(Town town) {
         TM.remove(town.getName());
     }
-    //
 
+    //
+    public Rank getRank(String name) {
+        return RankList.get(name);
+    }
 //MAPPING
 
-    //ENABLE DISABLE
 
+    //ENABLE DISABLE
     @Override
     public void onEnable() {
         instance = this;
         // Plugin startup logic
-        registerCMD("towns",new TownCmd());
-        registerCMD("plot",new PlotCmd());
+        registerCMD("towns", new TownCmd());
+        registerCMD("plot", new PlotCmd());
         regListen(new InvClickListen());
 
+        ConfigManager.get.LoadUp();
+    }
+
+
+    public void RegisterRanks(Rank rank) {
+        RankList.put(rank.getName(), rank);
     }
 
     @Override
@@ -132,9 +163,6 @@ public void regListen(Listener lis){
     }
 
     //ENABLE DISABLE
-
-
-
 
 
 }
--- src/main/java/com/rocketmail/vaishnavanil/towns/Towns/Town.java
+++ src/main/java/com/rocketmail/vaishnavanil/towns/Towns/Town.java
@@ -13,97 +13,109 @@ import java.util.UUID;
 public class Town {
     private String town_name;
     private UUID Mayor_ID;
-    private HashMap<Rank,List<UUID>> rankMap = new HashMap<>();
+    private HashMap<UUID, Rank> rankMap = new HashMap<>();
     private List<UUID> Members = new ArrayList<>();
     private List<Town> Allies = new ArrayList<>();
     private List<Town> Enemies = new ArrayList<>();
-    public String getName(){
+
+    public String getName() {
         return town_name;
     }
-    public Player getMayor(){
+
+    public Player getMayor() {
         return Bukkit.getPlayer(Mayor_ID);
     }
 
-    public void setRank(Player p,Rank rank){
-        List<UUID> idList = null;
-        try {
-            idList = rankMap.get(rank) == null ? new ArrayList<>() : rankMap.get(rank);
-        }catch(Exception e){
-            idList = new ArrayList<>();
-
-        }
-        idList.add(p.getUniqueId());
-        for(Rank r:rankMap.keySet()){
-            if(rankMap.get(r).contains(p.getUniqueId())){
-                List<UUID> rEmList = null;
-                try {
-                    rEmList = rankMap.get(r) == null ? new ArrayList<>() : rankMap.get(r);
-                }catch(Exception e){
-                    rEmList = new ArrayList<>();
-
-                }
-                rEmList.remove(p.getUniqueId());
-                rankMap.put(r,rEmList);
-            }
-        }
-        rankMap.put(rank, idList);
+    public void setRank(Player p, Rank rank) {
+        rankMap.put(p.getUniqueId(), rank);
     }
 
-    public void claim(Chunk chunk,Player owner){
-        new Claim(chunk,this,owner);
+    public Rank getRank(Player p) {
+        return rankMap.get(p.getUniqueId());
     }
 
-    public void unclaim(Chunk chunk){
+    public Rank getRank(UUID id) {
+        return rankMap.get(id);
+    }
+
+    public boolean hasRank(Player p) {
+        return rankMap.keySet().contains(p.getUniqueId());
+    }
+
+    public boolean hasRank(UUID id) {
+        return rankMap.keySet().contains(id);
+    }
+
+    public boolean hasPermission(String perm, Player player) {
+        if (!hasRank(player)) return false;
+        return getRank(player).hasPermission(perm);
+    }
+
+    public void claim(Chunk chunk, Player owner) {
+        new Claim(chunk, this, owner);
+    }
+
+    public void unclaim(Chunk chunk) {
         TownS.g().rCfT(TownS.g().getClaim(chunk));
     }
-    public void Ally(Town t){
+
+    public void Ally(Town t) {
         unEnemy(t);
         Allies.add(t);
     }
-    public void Enemy(Town t){
+
+    public void Enemy(Town t) {
         unAlly(t);
         Enemies.add(t);
     }
-    public void unAlly(Town t){
+
+    public void unAlly(Town t) {
         Allies.remove(t);
     }
-    public void unEnemy(Town t){
+
+    public void unEnemy(Town t) {
         Enemies.remove(t);
     }
-    public void deleteTown(){
+
+    public void deleteTown() {
         TownS.g().rPfT(Mayor_ID);
 
-        for(UUID a:rankMap.get(new Rank("",new ArrayList<>()))){
+        for (UUID a : rankMap.keySet()) {
             TownS.g().rPfT(a);
         }
-        for(UUID m:Members){
+        for (UUID m : Members) {
             TownS.g().rPfT(m);
         }
         TownS.g().rTfM(this);
     }
 
-    public boolean belongs(Player player){
-        if(Members.contains(player.getUniqueId()) /*|| Assistants.contains(player.getUniqueId()) */|| Mayor_ID == player.getUniqueId())return true;
+    public boolean belongs(Player player) {
+        if (Members.contains(player.getUniqueId()) || rankMap.containsKey(player.getUniqueId()) || Mayor_ID == player.getUniqueId())
+            return true;
         return false;
     }
-    public Town(String name,Player mayor){
+
+    public Town(String name, Player mayor) {
         town_name = name;
         Mayor_ID = mayor.getUniqueId();
         TownS.g().aTtM(this);
     }
-    public Town(String name,Player mayor,List<UUID> MemberL,List<UUID> AssistantL){
+
+    public Town(String name, Player mayor, List<UUID> MemberL, List<UUID> AssistantL) {
         town_name = name;
         Mayor_ID = mayor.getUniqueId();
         Members = MemberL;
-       //Assistants = AssistantL;
+        //Assistants = AssistantL;
         TownS.g().aTtM(this);
     }
-    public Town(String name,Player mayor,List<UUID> MemberL,List<UUID> AssistantL,List<Claim> claims){
+
+    public Town(String name, Player mayor, List<UUID> MemberL, List<UUID> AssistantL, List<Claim> claims) {
         town_name = name;
         Mayor_ID = mayor.getUniqueId();
         Members = MemberL;
-       // Assistants = AssistantL;
-        for(Claim c:claims){
+        // Assistants = AssistantL;
+
+        for (Claim c : claims) {
             TownS.g().aCtT(c);
         }
         TownS.g().aTtM(this);
