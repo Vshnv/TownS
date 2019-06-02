99379dbf0cc3e327f7acc1f946e14085e7010ff3
Rishabh Raghunath <rishabh.r@zohocorp.com>
2019-06-02 20:52:52 +0530

    bug fix

--- src/main/java/com/rocketmail/vaishnavanil/towns/Economy/EconomyHandler.java
+++ src/main/java/com/rocketmail/vaishnavanil/towns/Economy/EconomyHandler.java
@@ -9,40 +9,45 @@ import org.bukkit.entity.Player;
 public enum EconomyHandler {
     INSTANCE;
 
+    /* Method to add and remove player balances */
     public Boolean changePlayerBalance(Player player, Integer amount) {
         if (amount > 0) {
             return TownS.getEconomy().depositPlayer(player, amount).transactionSuccess();
         } else {
-            return TownS.getEconomy().withdrawPlayer(player, amount).transactionSuccess();
+            return TownS.getEconomy().withdrawPlayer(player, Math.abs(amount)).transactionSuccess();
         }
     }
-
-    public Boolean createTownBank(Town town) {
-        return TownS.getEconomy().createBank(town.getTownUUID().toString(), town.getMayor()).transactionSuccess();
-    }
-
-    public Boolean deleteTownBank(Town town){
-        return TownS.getEconomy().deleteBank(town.getTownUUID().toString()).transactionSuccess();
-    }
-
+    /* Method to add and remove town bank balances */
     public Boolean changeTownBalance(Town town, Integer amount) {
         if (amount > 0) {
             return TownS.getEconomy().bankDeposit(town.getTownUUID().toString(), amount).transactionSuccess();
         } else {
-            return TownS.getEconomy().bankWithdraw(town.getTownUUID().toString(), amount).transactionSuccess();
+            return TownS.getEconomy().bankWithdraw(town.getTownUUID().toString(), Math.abs(amount)).transactionSuccess();
         }
     }
 
-    public Double getTownBalance(Town town) {
-        return TownS.getEconomy().bankBalance(town.getTownUUID().toString()).amount;
+    /* Method to create a town's bank. Used on initial town creation. */
+    public Boolean createTownBank(Town town) {
+        return TownS.getEconomy().createBank(town.getTownUUID().toString(), town.getMayor()).transactionSuccess();
     }
 
+    /* Method to remove town's bank */
+    public Boolean deleteTownBank(Town town){
+        return TownS.getEconomy().deleteBank(town.getTownUUID().toString()).transactionSuccess();
+    }
+
+    /* Method to get player's bank balance */
     public Double getPlayerBalance(Player player) {
         if (player.hasPlayedBefore()) {
             return TownS.getEconomy().getBalance(player);
         } else {
             return 0.0;
         }
+    }
+
+    /* Method to get town bank balance */
+    public Double getTownBalance(Town town) {
+        return TownS.getEconomy().bankBalance(town.getTownUUID().toString()).amount;
     }
 
 }
