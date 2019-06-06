b1de5912bef31b0a39ee54b71b9a01c6895dadb2
Rishabh Raghunath <rishabh.r@zohocorp.com>
2019-06-07 00:16:06 +0530

    completer classes

---
+++ src/main/java/com/rocketmail/vaishnavanil/towns/Commands/AutoComplete/PlotCmdCompleter.java
@@ -0,0 +1,14 @@
+package com.rocketmail.vaishnavanil.towns.Commands.AutoComplete;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.List;
+
+public class PlotCmdCompleter implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        return null;
+    }
+}
