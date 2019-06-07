9293a06a834b2935b69de9272259d596f63d4ef8
Rishabh Raghunath <rishabh.r@zohocorp.com>
2019-06-08 02:02:36 +0530

    wilderness cancel interract

---
+++ src/main/java/com/rocketmail/vaishnavanil/towns/Commands/AutoComplete/WildernessInteractListener.java
@@ -0,0 +1,28 @@
+package com.rocketmail.vaishnavanil.towns.Commands.AutoComplete;
+
+import com.rocketmail.vaishnavanil.towns.Messages.Format;
+import com.rocketmail.vaishnavanil.towns.TownS;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class WildernessInteractListener implements Listener {
+
+    @EventHandler
+    public void onWildernessInteract(PlayerInteractEvent event){
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+        if(block == null) return;
+        if(block.getType().equals(Material.AIR)) return;
+        if(block.getWorld().getName().equals("world")){
+            if(!TownS.g().isClaimed(block.getLocation().getChunk())){
+                event.setCancelled(true);
+                Format.CmdErrFrmt.use().a(player, "This Area does not belong to you yet!");
+            }
+        }
+    }
+
+}
