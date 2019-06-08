53c8dd21c49fe261edf263a7be37c2decacbf5bd
Rishabh Raghunath <rishabh.r@zohocorp.com>
2019-06-08 14:11:14 +0530

    cancel only blockplace and break

--- src/main/java/com/rocketmail/vaishnavanil/towns/Commands/AutoComplete/WildernessInteractListener.java
+++ src/main/java/com/rocketmail/vaishnavanil/towns/Commands/AutoComplete/WildernessInteractListener.java
@@ -7,14 +7,48 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class WildernessInteractListener implements Listener {
 
+//    @EventHandler
+//    public void onWildernessInteract(PlayerInteractEvent event){
+//        Player player = event.getPlayer();
+//        Block block = event.getClickedBlock();
+//        if(block == null) return;
+//        if(block.getType().equals(Material.AIR)) return;
+//        if(block.getWorld().getName().equals("world")){
+//            if(!TownS.g().isClaimed(block.getLocation().getChunk())){
+//                if(!player.hasPermission("towns.buildwild")){
+//                    event.setCancelled(true);
+//                    Format.CmdErrFrmt.use().a(player, "This Area does not belong to you yet!");
+//                }
+//            }
+//        }
+//    }
+
     @EventHandler
-    public void onWildernessInteract(PlayerInteractEvent event){
+    public void onBlockBreakWild(BlockBreakEvent event){
         Player player = event.getPlayer();
-        Block block = event.getClickedBlock();
+        Block block = event.getBlock();
+        if(block == null) return;
+        if(block.getType().equals(Material.AIR)) return;
+        if(block.getWorld().getName().equals("world")){
+            if(!TownS.g().isClaimed(block.getLocation().getChunk())){
+                if(!player.hasPermission("towns.buildwild")){
+                    event.setCancelled(true);
+                    Format.CmdErrFrmt.use().a(player, "This Area does not belong to you yet!");
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlaceWild(BlockPlaceEvent event){
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
         if(block == null) return;
         if(block.getType().equals(Material.AIR)) return;
         if(block.getWorld().getName().equals("world")){
