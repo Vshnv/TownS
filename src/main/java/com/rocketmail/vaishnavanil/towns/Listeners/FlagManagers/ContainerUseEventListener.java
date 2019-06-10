698e931d7ce4fc607869578a71fd9a8daafd6bbe
Rishabh Raghunath <rishabh.r@zohocorp.com>
2019-06-10 23:58:57 +0530

    container access fix

--- src/main/java/com/rocketmail/vaishnavanil/towns/Listeners/FlagManagers/ContainerUseEventListener.java
+++ src/main/java/com/rocketmail/vaishnavanil/towns/Listeners/FlagManagers/ContainerUseEventListener.java
@@ -5,6 +5,8 @@ import com.rocketmail.vaishnavanil.towns.Messages.Format;
 import com.rocketmail.vaishnavanil.towns.TownS;
 import com.rocketmail.vaishnavanil.towns.Towns.Claim;
 import com.rocketmail.vaishnavanil.towns.Towns.Flag;
+import com.rocketmail.vaishnavanil.towns.Towns.Town;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,44 +16,73 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 public class ContainerUseEventListener implements Listener {
     @EventHandler
-    public void onContainerUse(PlayerInteractEvent e) {
-        if (!(e.getAction() == Action.PHYSICAL || e.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
+    public void onContainerUse(PlayerInteractEvent e){
+        if(!(e.getAction() == Action.PHYSICAL || e.getAction() == Action.RIGHT_CLICK_BLOCK))return;
 
-        if (!TownS.g().isClaimed(e.getClickedBlock().getLocation().getChunk())) return;
+        if(!TownS.g().isClaimed(e.getClickedBlock().getLocation().getChunk()))return;
 
-        if (Constraints.Container.isRestricted(e.getClickedBlock().getType())) {
+        if(Constraints.Container.isRestricted(e.getClickedBlock().getType())){
 
             Player player = e.getPlayer();
             Claim claim = TownS.g().getClaim(e.getClickedBlock().getLocation().getChunk());
 
-            if (claim.getTown().belongs(player)) {
+            if(claim.getTown().belongs(player)){
 
-                if (claim.hasFlag(Flag.CONTAINER)) {
+                if(claim.canUseContainer(e.getPlayer()))return;
+
+                if(claim.hasFlag(Flag.CONTAINER)){
                     return;
-                } else {
-                    if (claim.getOwner().getUniqueId().equals(player.getUniqueId())) {
+                }else{
+
+                    if(claim.getOwner().getUniqueId().equals(player.getUniqueId())){
                         return;
-                    } else {
-                        if (claim.getTown().hasPermission("Allow.ContainerALL", e.getPlayer())) {
+                    }else{
+                        if(claim.getTown().hasPermission("Allow.ContainerALL",e.getPlayer())){
                             return;
-                        } else {
-                            if (claim.getTown().hasPermission("ContainerALL", player.getUniqueId())) {
-                                player.sendMessage(ChatColor.RED + "Opening container using override permission.");
+                        }else{
+                            if(claim.getTown().hasPermission("ContainerALL", player.getUniqueId())){
+                                player.sendMessage(ChatColor.RED+"Opening container using override permission.");
                                 return;
                             }
                             e.setCancelled(true);
                             Format.CmdErrFrmt.use().a(player, "You lack permission to open containers here!");
                         }
                     }
+
+
                 }
 
-            } else {
+            }else{
                 e.setCancelled(true);
                 Format.CmdErrFrmt.use().a(player, "You do not belong to this town!");
             }
 
         }
 
-
+//            Claim claim = TownS.g().getClaim(e.getClickedBlock().getLocation().getChunk());
+//                if(claim.hasFlag(Flag.CONTAINER)){
+//                    if(!claim.getTown().belongs(e.getPlayer())){
+//                       e.setCancelled(true);
+//                       return;
+//                    }
+//                }else{
+//
+//                    if(claim.getTown().hasPermission("Allow.ContainerALL",e.getPlayer())){
+//                        return;
+//                    }
+//
+//                    if(!claim.getTown().belongs(e.getPlayer())){
+//                        Bukkit.broadcastMessage("else trigged evennt");
+//
+//
+//                        if(claim.canUseContainer(e.getPlayer()))return;
+//                        e.setCancelled(true);
+//                        return;
+//                    }
+//
+//
+//                }
+//
+//        }
     }
 }
